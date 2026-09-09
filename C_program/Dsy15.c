fb040187216c0b40352897945831d425267cdcef
anuradhajadhav1346-netizen <anuradhajadha1346@gamil.com>
2026-09-08 18:26:13 -1200

    Add 15th program

---
+++ C_program/Dsy15.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    char op;
+    printf("Enter expressions:");
+    scanf("%c",&op);
+    printf("Enter any two numbers:");
+    scanf("%f %f",&a,&b);
+    int result = printf("Answers of your expression is = %d",calculate (a,b,op));
+    return 0;
+}
+
+int calculate(float a,float b,char op)
+{
+    switch(op)
+    {
+        case'+': 
+                    return a+b;
+        case '-':
+                    return a-b;
+        case '*':
+                    return a*b;
+        case '/':
+                    if(b!=0)
+                    {
+                        return a/b;
+                    }
+                    else
+                    {
+                        printf("Division is not allowed");
+                        break;
+                    }
+        default: pritnf("Invalied opration");
+    }
+    return 0;
+}
