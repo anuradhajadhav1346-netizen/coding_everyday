bae0c777eb66a631f61dc7c1c7ace717d39f0a3b
anuradhajadhav1346-netizen <anuradhajadhav1346@gmail.com>
2026-09-09 20:18:46 -1200

    Add 16th program

---
+++ C_program/Day16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    float a,b ;
+    float result;
+    printf("Enter 2 numbers=");
+    scnaf("%f %f",&a,&b);
+    result= printf("Calculation = %f",calculator(a,b));
+}
+
+float calculator(float a, float b)
+{
+    if(b==0)
+    {
+        error=1;
+    }
+    else
+    {
+        return a/b;
+    }
+}
