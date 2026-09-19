cb8cd9eadff3ebfd9f5549c48d1fea981873f990
anuradhajadhav1346-netizen <anuradhajadhav1346@gmai.com>
2026-09-18 18:32:20 -1200

    Add 23th program

---
+++ C_program/Day23.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],i,ch;
+    printf("Enter string=");
+    gets(str);
+    printf("Enter character=");
+    scanf("%c",&ch);
+    int count = 0;
+    for(i=0; str[i]!='\0'; i++)
+    {
+        if(str[i]==ch || str[i]== ch+32 || str[i]== ch - 32)
+        {  
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
