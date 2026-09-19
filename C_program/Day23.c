feb225e67d82fdd1f6142952bb97b4674fbc8f93
anuradhajadhav1346-netizen <anuradhajadhav1346@gmai.com>
2026-09-18 18:36:03 -1200

    modified 23th program

--- C_program/Day23.c
+++ C_program/Day23.c
@@ -3,11 +3,11 @@
 int main()
 {
     char str[100],i,ch;
+    int count=0;
     printf("Enter string=");
     gets(str);
     printf("Enter character=");
     scanf("%c",&ch);
-    int count = 0;
     for(i=0; str[i]!='\0'; i++)
     {
         if(str[i]==ch || str[i]== ch+32 || str[i]== ch - 32)
