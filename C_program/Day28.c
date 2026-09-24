0e80df0d4d123330d4c016cae172a45eda2de4b0
anuradhajadhav1346-netizen <anuradhajadhav1346@gmail.com>
2026-09-23 20:30:02 -1200

    modified 28th program

--- C_program/Day28.c
+++ C_program/Day28.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main (int argc[])
+int main(int argc[])
 {
     printf("argc = %d\n",argc);
-    float *mark;
-    mark=(float *)malloc(sizeof(float) *5);
+    float *marks;
+    marks=(float *)malloc(sizeof(float) *5);
     int i;
-    if(mark==NULL)
+    if(marks==NULL)
     {
         printf("memory allocation is failed..\n");
         return 1;
@@ -14,13 +14,13 @@ int main (int argc[])
     printf("Enter marks of sunjects :");
     for (i=0; i<5; i++)
     {
-        scanf("%f",&mark[i]);
+        scanf("%f",&marks[i]);
     }
     for(i=0; i<5; i++)
     {
-        printf("arr[%d] : %.2f\n",i,mark[i]);
+        printf("arr[%d] : %.2f\n",i,marks[i]);
     }
-    free(mark);
+    free(marks);
     marks=NULL;
     return 0;
 }
